85f26948ad8e60247376806366a989eb08a45482
Dan Henke <dan@thinkalike.com>
2021-03-30 21:03:53 +1000

    change to docker context description

--- init_docker.sh
+++ init_docker.sh
@@ -4,7 +4,7 @@
 [ -z "$SERVER_DOMAIN" ] && { echo "ERROR: Missing \$SERVER_DOMAIN"; exit 1; }
 
 # Create a docker context
-docker context create ${SERVER_NAME} --default-stack-orchestrator swarm --description "${SERVER_NAME} @ ${SERVER_DOMAIN}" --docker host=ssh://root@${SERVER_DOMAIN}
+docker context create ${SERVER_NAME} --default-stack-orchestrator swarm --description "${SERVER_DOMAIN}" --docker host=ssh://root@${SERVER_DOMAIN}
 
 # Initialize docker swarm
 docker --context ${SERVER_NAME} swarm init --advertise-addr 127.0.0.1
